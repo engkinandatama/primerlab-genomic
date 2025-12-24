a84cdc0d7e6105143ca21fa343cbddaa420ea2d3
engkinandatama <engkinandatama@outlook.com>
2025-12-24 15:59:13 +0700

    feat(v0.3.4): export new insilico functions and fix version tests

--- primerlab/core/insilico/__init__.py
+++ primerlab/core/insilico/__init__.py
@@ -1,11 +1,17 @@
 """
-In-silico PCR Simulation Module (v0.2.2)
+In-silico PCR Simulation Module (v0.3.4)
 
 Virtual PCR simulation for amplicon prediction and primer validation.
 """
 
 from primerlab.core.insilico.engine import InsilicoPCR, run_insilico_pcr
-from primerlab.core.insilico.binding import BindingSite, analyze_binding
+from primerlab.core.insilico.binding import (
+    BindingSite,
+    analyze_binding,
+    calculate_corrected_tm,      # v0.3.4
+    check_three_prime_stability, # v0.3.4
+    calculate_three_prime_dg,
+)
 from primerlab.core.insilico.report import (
     generate_markdown_report,
     generate_amplicon_fasta,
@@ -17,6 +23,9 @@ __all__ = [
     "run_insilico_pcr",
     "BindingSite",
     "analyze_binding",
+    "calculate_corrected_tm",       # v0.3.4
+    "check_three_prime_stability",  # v0.3.4
+    "calculate_three_prime_dg",
     "generate_markdown_report",
     "generate_amplicon_fasta",
     "format_console_alignment",
